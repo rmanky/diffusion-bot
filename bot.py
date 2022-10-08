942d9e4a431a0c31cffa40aaa7836cdeb02a205b
rmanky <rmankaryous12@gmail.com>
2022-10-08 16:42:39 -0700

    Revert "fixes"
    This reverts commit f9c1d102cd10813d239893607f29ee0d25cbaf6b.

--- bot.py
+++ bot.py
@@ -23,16 +23,13 @@ CHANGE_LIST = {
     - It uses the Euler Ancestral sampler at 32 steps, which can give more _detailed_ outputs
     - The queue can be quite long, the bot will check it every `max(wait * 0.1, 5)` seconds
     """,
-    "NSFW Warning": """
-    - Images contained in an embed can **not** be marked as a spoiler (blame Discord)
-    - Yes I've tried "ballsack", it works as expected...
-    """,
     "Simplified Commands": """
     - The `fix` parameter has been removed from `/dream` for simplicity, please use `/fix` instead
     - Commands now use a unified generic error handler
     """,
-    "Goodbye Fly, Hello Render": """
-    - Switched to Render for backend hosting, Fly was fly but just couldn't fly
+    "NSFW Warning": """
+    - Images contained in an embed can **not** be marked as a spoiler (blame Discord)
+    - Yes I've tried "ballsack", it works as expected...
     """,
 }
 
@@ -144,13 +141,12 @@ async def horde(inter, prompt: str):
     image_bytes = await stable_horde_get(id)
     file = File(
         io.BytesIO(base64.b64decode(image_bytes)),
-        filename=f"result.webp",
+        filename=f"{prompt.replace(' ','_')}.webp",
     )
-
     embed.clear_fields()
     embed.title = "✅ Completed"
-    embed.set_image(file=file)
     embed.add_field(name="Prompt", value=prompt, inline=False)
+    embed.set_image(file=file)
     await inter.edit_original_response(embed=embed)
 
 
